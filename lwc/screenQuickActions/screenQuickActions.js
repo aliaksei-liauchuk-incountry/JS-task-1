dde967054f9a5399d3b730b927d9182f9b60e9d2
ViktorPenzar <penzar@mail.ru>
2022-08-11 12:40:05 +0300

    JS task 1

--- lwc/screenQuickActions/screenQuickActions.js
+++ lwc/screenQuickActions/screenQuickActions.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, wire } from "lwc";
+import { LightningElement, api } from "lwc";
 import { CloseActionScreenEvent } from 'lightning/actions';
 export default class RecordContextAction extends LightningElement {
 	@api recordId;
