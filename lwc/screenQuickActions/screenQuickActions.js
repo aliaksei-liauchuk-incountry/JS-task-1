38fd54d26b619eae3ed116680ce4f3900d6dab04
ViktorPenzar <penzar@mail.ru>
2022-08-18 14:37:28 +0300

    Syntax is fixed

--- lwc/screenQuickActions/screenQuickActions.js
+++ lwc/screenQuickActions/screenQuickActions.js
@@ -1,15 +1,17 @@
 import { LightningElement, api } from "lwc";
 import { CloseActionScreenEvent } from 'lightning/actions';
+
 export default class RecordContextAction extends LightningElement {
-	@api recordId;
-	@api objectApiName;
-	getInfo = false;
 
-	handleGetInfo() {
-		this.getInfo = true;
-	}
+    @api recordId;
+    @api objectApiName;
+    getInfo = false;
 
-	handleCancel() {
-		this.dispatchEvent(new CloseActionScreenEvent());
-	}
+    handleGetInfo() {
+        this.getInfo = true;
+    }
+
+    handleCancel() {
+        this.dispatchEvent(new CloseActionScreenEvent());
+    }
 }
