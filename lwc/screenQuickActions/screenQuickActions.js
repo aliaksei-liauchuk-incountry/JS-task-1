c9fcf8fada141d507fb394c47126d492e87a862b
ViktorPenzar <penzar@mail.ru>
2022-08-18 23:09:41 +0300

    Fised syntax erors

--- lwc/screenQuickActions/screenQuickActions.js
+++ lwc/screenQuickActions/screenQuickActions.js
@@ -1,7 +1,7 @@
 import { LightningElement, api } from "lwc";
 import { CloseActionScreenEvent } from 'lightning/actions';
 
-export default class RecordContextAction extends LightningElement {
+export default class ScreenQuickActions extends LightningElement {
 
     @api recordId;
     @api objectApiName;
